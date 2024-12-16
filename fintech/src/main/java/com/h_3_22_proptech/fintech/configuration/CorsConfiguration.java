b5ed0bd2c1345d8b8bac13c3347cd044ba6b7731
ExequielBaez <exequielbaez@gmail.com>
2024-12-16 13:02:36 -0300

    cors

--- fintech/src/main/java/com/h_3_22_proptech/fintech/configuration/CorsConfiguration.java
+++ fintech/src/main/java/com/h_3_22_proptech/fintech/configuration/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOrigins("https://h3-22-proptech.vercel.app/")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
